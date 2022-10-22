a53dfd3f00b784bbb5012fcb3636757e71e3c7df
Yakovlev Mikhail Alekseevich <yakovmikemikle@gmail.com>
2022-10-22 17:09:50 +0300

    refactor: css-modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
-import './App.css'
-import { Footer } from './components/footer/Footer'
+import styles from './App.module.css'
+import { Footer } from './components/footer/footer'
 
 function App() {
 	return (
-		<div className="App">
-			<div className="App-welcome">
+		<div className={styles.app}>
+			<div className={styles.appWelcome}>
 				<p>Welcome.</p>
-				<p>My name is Vlad Repinskiy, I am a <span style={{color: '#FF0000'}}>Fullstack developer</span> in the making.</p>
+				<p>
+					My name is Vlad Repinskiy, I am a
+					&nbsp;<span className={styles.highlighted}>Fullstack developer</span>
+					&nbsp;in the making.
+				</p>
 				<p>
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dignissim leo erat, non tempus urna imperdiet id.
-					Pellentesque sit amet tristique sem. Sed eu <span style={{color: '#FF0000'}}>metus</span> at est venenatis pharetra. Sed in rutrum elit, euismod condimentum sem.
+					Pellentesque sit amet tristique sem. Sed eu <span className={styles.highlighted}>metus</span> at est venenatis pharetra. Sed in rutrum elit, euismod condimentum sem.
 					Aliquam vel nunc ultrices dolor porttitor finibus. Donec bibendum molestie aliquet.
 					Nunc vel tellus pretium, aliquet felis ultrices, suscipit magna.
-					Nullam dignissim magna metus, at sagittis <span style={{color: '#FF0000'}}>ligula</span> feugiat vitae.
+					Nullam dignissim magna metus, at sagittis <span className={styles.highlighted}>ligula</span> feugiat vitae.
 					Morbi vitae sem auctor, pretium quam ac, dignissim erat. Fusce a mauris metus.
 					Fusce sed sem eu elit gravida congue non at diam. Aliquam interdum neque enim.
 					Quisque et lacus iaculis, viverra libero interdum, ultricies.
