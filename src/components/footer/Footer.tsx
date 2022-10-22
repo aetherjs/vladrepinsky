29a0716d8fa84fd6d49928cf073d146f97b44b9d
Yakovlev Mikhail Alekseevich <yakovmikemikle@gmail.com>
2022-10-22 16:17:39 +0300

    add linter and fix code style

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,17 +1,17 @@
-import * as React from "react";
+import * as React from 'react'
 
 export function Footer() {
-  return (
-    <footer>
-            <ul>
-              <li>Links</li>
-              <li><a href="https://github.com/aetherjs">GitHub</a></li>
-              <li>And</li>
-              <li>Some</li>
-              <li>More</li>
-              <li>Links</li>
-              <li>@aetherjs {new Date().getFullYear()}</li>
-            </ul>
-    </footer>
-  );
+	return (
+		<footer>
+			<ul>
+				<li>Links</li>
+				<li><a href="https://github.com/aetherjs">GitHub</a></li>
+				<li>And</li>
+				<li>Some</li>
+				<li>More</li>
+				<li>Links</li>
+				<li>@aetherjs {new Date().getFullYear()}</li>
+			</ul>
+		</footer>
+	)
 }
